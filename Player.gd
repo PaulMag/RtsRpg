cdcb54a8f83f7c1917b28a4044e99cb630d5316e
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2023-05-07 17:01:59 +0200

    Move unit selection to Player script

--- Player.gd
+++ Player.gd
@@ -6,6 +6,14 @@ extends Node
 
 @export var isCloning = false
 
+const SELECTION_BOX_MINIMUM_SIZE = 5
+
+var allUnits = []
+var isDraggingMouse = false
+var isSelecting = false
+var selectionStart = Vector2()
+var selectionEnd = Vector2()
+
 
 func _ready():
 	playerId = name.to_int()
@@ -17,7 +25,12 @@ func _ready():
 	else:
 		print("Player on peer:   ", playerId, "  '", name, "'  ")
 
+
 func _process(delta):
+	
+	if isDraggingMouse:
+		draw_selection_box()
+		
 #	$"..".playerId = multiplayer.get_unique_id()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -32,6 +45,62 @@ func _process(delta):
 func clone():
 	isCloning = true
 
+
+func area_selected(start, end):
+	var area = []
+	area.append(Vector2(min(start.x, end.x), min(start.y, end.y)))
+	area.append(Vector2(max(start.x, end.x), max(start.y, end.y)))
+	set_selection_area(area)
+
+func _input(event):
+	event.is_action_pressed("mouse_left_click")
+	if event.is_action_pressed("mouse_left_click"):
+		selectionStart = $Panel.get_global_mouse_position()
+		isDraggingMouse = true	
+		$Panel.visible = true
+	if isDraggingMouse:
+		selectionEnd = $Panel.get_global_mouse_position()
+	if event.is_action_released("mouse_left_click"):
+		selectionEnd = $Panel.get_global_mouse_position()
+		isDraggingMouse = false
+		$Panel.visible = false
+		area_selected(selectionStart, selectionEnd)
+
+func _on_selection_timer_timeout():
+	isSelecting = false
+	$SelectionDetector/CollisionShape2D.disabled  = true
+
+func set_selection_area(area):
+	allUnits = get_tree().get_nodes_in_group("units")
+	for unit in allUnits:
+		unit.set_selected(false)
+	selectedUnits = []
+	isSelecting = true
+	$SelectionDetector/CollisionShape2D.disabled  = false
+	$SelectionDetector.position = (area[0] + area[1]) * 0.5
+	$SelectionDetector/CollisionShape2D.shape.size.x = abs(area[0].x - area[1].x)
+	$SelectionDetector/CollisionShape2D.shape.size.y = abs(area[0].y - area[1].y)
+	$SelectionTimer.start()
+
+func _on_selection_detector_area_entered(area):
+	if isSelecting:
+		var unit = area.get_parent()
+		unit.set_selected(true)
+		selectedUnits.append(unit)
+		print(selectedUnits)
+		#TODO: Should select according to z_index ordering
+		var boxShape = abs(selectionStart - selectionEnd)
+		if boxShape.x + boxShape.y < SELECTION_BOX_MINIMUM_SIZE:
+			isSelecting = false
+			$SelectionDetector/CollisionShape2D.disabled  = true
+
+func draw_selection_box(s=true):
+	$Panel.size = Vector2(abs(selectionStart.x - selectionEnd.x), abs(selectionStart.y - selectionEnd.y))
+	var pos = Vector2()
+	pos.x = min(selectionStart.x, selectionEnd.x)
+	pos.y = min(selectionStart.y, selectionEnd.y)
+	$Panel.position = pos
+	$Panel.size *= int(s)
 
 
 
@@ -52,3 +121,5 @@ func clone():
 #	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 #	if(direction):
 #		print($"..".playerId, "   ", direction)
+
+
