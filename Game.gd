eb7a6bf97016fe868633a30d00da035a31ca96aa
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2023-05-09 21:34:13 +0200

    Move order happens through Game script on server

--- Game.gd
+++ Game.gd
@@ -4,8 +4,13 @@ func _physics_process(delta):
 	var players = $Multiplayer/Players.get_children()
 	var units = $Dungeon/Units.get_children()
 	for player in players:
+		if player.isIssuingMoveOrder:
+			for unitId in player.selectedUnitIds:
+				var unit = instance_from_id(unitId)
+				if unit.playerId == player.playerId:
+					unit.move_to(player.destination)
+			player.isIssuingMoveOrder = false
 		for unit in units:
-#			print("player %s,  unit %s,  direction %s" % [player.name, unit.playerId, player.direction])
 			if player.name.to_int() == unit.playerId:
 				unit.position += player.direction * delta * unit.SPEED
 				if player.isCloning:
