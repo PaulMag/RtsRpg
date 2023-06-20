765c5344eb1e67ebd42c617e771b43e554f927a1
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2023-06-20 20:06:50 +0200

    Add PlayerLabel and fiddle with UI

--- Multiplayer.gd
+++ Multiplayer.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const PLAYER = preload("res://Player.tscn")
+const PLAYER_LABEL = preload("res://PlayerLabel.tscn")
 
 const PORT = 4433
 
@@ -16,21 +17,21 @@ func _ready():
 	if DisplayServer.get_name() == "headless":
 		print("Automatically starting dedicated server.")
 		_on_host_pressed.call_deferred()
-		
+
 func _on_host_pressed():
 	# Start game as server
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT)
 	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
 		OS.alert("Failed to start multiplayer server.")
-		print("Failed to start multiplayer server.")		
+		print("Failed to start multiplayer server.")
 		return
 	multiplayer.multiplayer_peer = peer
 #	start_game()
 
 func _on_connect_pressed():
 	# Start game as client, and join existing host
-	var txt : String = $UI/Net/Options/Remote.text
+	var txt : String = $UI/Options/Joining/Remote.text
 	if txt == "":
 		OS.alert("Need a remote to connect to.")
 		return
@@ -44,28 +45,36 @@ func _on_connect_pressed():
 
 
 func start_game():
-	
+
 	$UI.hide()
 	print("Game started")
 	get_tree().paused = false
-	
+
 	var playerd2id : int
-	
+
 	if multiplayer.is_server():
 #		multiplayer.peer_connected.connect(add_player)
 #		multiplayer.peer_disconnected.connect(delete_player)
-		
+
 		for id in multiplayer.get_peers():
 			add_player(id)
 			print("Added peer player %s." % id)
 			playerd2id = id
-			
-		
+
+
 		if not OS.has_feature("dedicated_server"):
 			add_player(1)
 			print("Not dedicated server. Added player 1.")
-	
+
 	$"../Dungeon/Units/Unit3".playerId = playerd2id
+
+
+	for player in $Players.get_children():
+		var playerLabelNode = PLAYER_LABEL.instantiate()
+		playerLabelNode.playerId = player.playerId
+		playerLabelNode.playerName = player.name
+		playerLabelNode.playerColor = player.playerColor
+		$PanelContainer/PlayerList.add_child(playerLabelNode, true)
 
 
 func _on_start_game_pressed():
