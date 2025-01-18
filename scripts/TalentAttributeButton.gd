6abfbc359687c33de510f813b0ad82a083e97e86
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2025-01-18 20:11:24 +0100

    Grey out Talent buttons when maxed out

--- scripts/TalentAttributeButton.gd
+++ scripts/TalentAttributeButton.gd
@@ -3,15 +3,3 @@ extends TalentBaseButton
 class_name TalentAttributeButton
 
 @export var attributes: Attributes
-@export var maxRank: int = 10
-@export var rank: int = 0
-
-func _ready() -> void:
-	nameLabel.text = talentName
-	rankLabel.text = "%d/%d" % [rank, maxRank]
-
-func rankUp() -> void:
-	rank += 1
-	rankLabel.text = "%d/%d" % [rank, maxRank]
-	if rank == maxRank:
-		disabled = true
--- scripts/TalentBaseButton.gd
+++ scripts/TalentBaseButton.gd
@@ -4,9 +4,24 @@ class_name TalentBaseButton
 
 @onready var nameLabel: Label = $NameLabel
 @onready var rankLabel: Label = $MarginContainer/RankLabel
+@onready var disableTexture: TextureRect = $MarginContainer/DisableTexture
+@export var maxRank: int = 1
+@export var rank: int = 0
 
 @export var talentName: String:
 	set(_talentName):
 		talentName = _talentName
 		if Engine.is_editor_hint() and nameLabel:
 			nameLabel.text = _talentName
+
+
+func _ready() -> void:
+	nameLabel.text = talentName
+	rankLabel.text = "%d/%d" % [rank, maxRank]
+
+func rankUp() -> void:
+	rank += 1
+	rankLabel.text = "%d/%d" % [rank, maxRank]
+	if rank == maxRank:
+		disabled = true
+		disableTexture.visible = true
