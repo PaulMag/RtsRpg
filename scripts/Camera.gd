3f2673b2564453706a295fef21ec63693a94e6cc
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2025-02-02 19:01:49 +0100

    Fix null errors and multiplayer WASD
    - Fixed bug where host WASD would control all players' cameras.
    - Fix problem where camera is null.
    - Fix bug where selected Unit is null when dead.

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -2,6 +2,10 @@ extends Camera2D
 
 
 func _process(_delta: float) -> void:
-	var selectedUnit := Global.getUnitFromUnitId(Global.getPlayerCurrent().selectedUnitId)
+	var currentPlayer := Global.getPlayerCurrent()
+	if currentPlayer == null:  #TODO: Workaround for bug.
+		return
+
+	var selectedUnit := Global.getUnitFromUnitId(currentPlayer.selectedUnitId)
 	if selectedUnit:
 		position = selectedUnit.position
