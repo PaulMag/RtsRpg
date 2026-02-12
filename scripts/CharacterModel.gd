5cbb74079399306ac9776173e135c3da8c46c0eb
Paul Magnus Sørensen-Clark <paulmag91@gmail.com>
2026-02-12 22:59:21 +0100

    Every CharacterModel has random head mesh and textures

--- scripts/CharacterModel.gd
+++ scripts/CharacterModel.gd
@@ -1,8 +1,11 @@
+# @tool
 extends Node3D
 
 class_name CharacterModel
 
 @export var defaultMeshes: Dictionary[Global.ItemSlots, Mesh]
+@export var headMeshes: Array[Mesh]
+@export var textures: Array[Texture2D]
 
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 @onready var itemMeshes: Dictionary[Global.ItemSlots, MeshInstance3D] = {
@@ -11,6 +14,35 @@ class_name CharacterModel
 	Global.ItemSlots.Head: %HeadItem,
 	Global.ItemSlots.Torso: %Torso,
 }
+@onready var headMeshInstance: MeshInstance3D = %Head
+@onready var meshInstances: Array[MeshInstance3D] = [
+	%Torso,
+	%ArmLeft,
+	%ArmRight,
+	%LegLeft,
+	%LegRight,
+	%BackItem,
+	%HeadItem,
+	%RightHandItem,
+	%LeftHandItem,
+]
+
+var clothesMaterial: StandardMaterial3D
+
+
+func _ready() -> void:
+	headMeshInstance.mesh = headMeshes.pick_random()
+	var material := StandardMaterial3D.new()
+	material.albedo_texture = textures.pick_random()
+	headMeshInstance.set_surface_override_material(0, material)
+
+	clothesMaterial = StandardMaterial3D.new()
+	clothesMaterial.albedo_texture = textures.pick_random()
+	for meshInstance in meshInstances:
+		meshInstance.set_surface_override_material(0, clothesMaterial)
+	for meshInstance: MeshInstance3D in meshInstances.slice(6, 9):
+		# These 3 only needed a mesh set to allow set_surface_override_material to work.
+		meshInstance.mesh = null
 
 
 func equipItem(item: Item) -> void:
